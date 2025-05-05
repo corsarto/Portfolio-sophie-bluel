1ae68e343797da05e609d498726abdc4867a334e
corsarto <clerc.loic@gmail.com>
2025-05-05 13:05:34 +0200

    fix add and delete picture refresh in the modal

--- FrontEnd/scripts/admin.js
+++ FrontEnd/scripts/admin.js
@@ -7,7 +7,6 @@ let currentModal = null;
 let urlAdmin = 'http://localhost:5678/api/';
 let previouslyFocusedElement = null;
 
-
 const galleryModalContent = document.getElementById('modal-content1');
 const addPictureModalContent = document.getElementById('modal-content2');
 
@@ -142,45 +141,45 @@ function containerModal(data) {
         }}
 
 function addProjectInGallery(projet) {
-            const gallery = document.querySelector(".gallery"); 
-            const figure = document.createElement("figure");
+    const gallery = document.querySelector(".gallery"); 
+    const figure = document.createElement("figure");
         
-            const img = document.createElement("img");
-            img.src = projet.imageUrl;
-            img.alt = projet.title;
+    const img = document.createElement("img");
+    img.src = projet.imageUrl;
+    img.alt = projet.title;
         
-            const figcaption = document.createElement("figcaption");
-            figcaption.textContent = projet.title;
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = projet.title;
         
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-            gallery.appendChild(figure);
-        }
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+}
 
 function addProjectInModal(projet) {
-            const pictureGallery = document.querySelector('.modal-gallery');
-            const figure = document.createElement("figure");
-            figure.classList.add("image-wrapper");
+    const pictureGallery = document.querySelector('.modal-gallery');
+    const figure = document.createElement("figure");
+    figure.classList.add("image-wrapper");
         
-            const img = document.createElement("img");
-            img.src = projet.imageUrl;
-            img.alt = projet.title;
+    const img = document.createElement("img");
+    img.src = projet.imageUrl;
+    img.alt = projet.title;
         
-            const trashIcon = document.createElement('i');
-            trashIcon.classList.add('fas', 'fa-trash-can', 'icon-overlay');
-            trashIcon.setAttribute('aria-hidden', 'false');
-            trashIcon.dataset.id = projet.id;
+    const trashIcon = document.createElement('i');
+    trashIcon.classList.add('fas', 'fa-trash-can', 'icon-overlay');
+    trashIcon.setAttribute('aria-hidden', 'false');
+    trashIcon.dataset.id = projet.id;
         
-            trashIcon.addEventListener('click', deleteProject);
+    trashIcon.addEventListener('click', deleteProject);
         
-            figure.appendChild(img);
-            figure.appendChild(trashIcon);
-            pictureGallery.appendChild(figure);
-        }
+    figure.appendChild(img);
+    figure.appendChild(trashIcon);
+    pictureGallery.appendChild(figure);
+}
 
 async function deleteProject(event) {
-            const id = event.target.dataset.id;
-            const token = localStorage.getItem("token");
+    const id = event.target.dataset.id;
+    const token = localStorage.getItem("token");
     
     if (!token) {
         console.error("Token manquant, impossible de supprimer ce projet.");
@@ -214,83 +213,82 @@ async function deleteProject(event) {
     
           }}
 
-    async function fetchCategoriesModal() {
-        try {
-            const response = await fetch(urlAdmin + 'categories');
-            if (!response.ok) {
-                throw new Error("Erreur dans la récupération des catégories");
-                    }
-                    const categories = await response.json();
-
-                    const defaultOption = document.createElement('option');
-                    defaultOption.value = '';
-                    defaultOption.disabled = true;  
-                    defaultOption.selected = true;  
-                    defaultOption.textContent = '';
-                    selectCat.appendChild(defaultOption);
-                    
-                    categories.forEach(cat => {
-                        const option = document.createElement('option');
-                        option.value = cat.id;
-                        option.textContent = cat.name;
-                        selectCat.appendChild(option);
-                    });
-                } catch (error) {
-                    console.error("Erreur : ", error);
+async function fetchCategoriesModal() {
+    try {
+        const response = await fetch(urlAdmin + 'categories');
+        if (!response.ok) {
+            throw new Error("Erreur dans la récupération des catégories");
                 }
-    }
-    fetchCategoriesModal();
-       
-        function resetForm() {
-            formAddTitle.value = '';
-            selectCat.selectedIndex = 0;
-            
-            const oldPreviewImage = addPictureWrapper.querySelector('img');
-            if (oldPreviewImage) {
-                oldPreviewImage.remove();
-            }
+        const categories = await response.json();
 
-            pictureIcon.setAttribute('style', 'display: flex');
-            labelBtnFile.setAttribute('style', 'display: flex');
-            pictureInstructionText.setAttribute('style', 'display: flex');
+        const defaultOption = document.createElement('option');
+        defaultOption.value = '';
+        defaultOption.disabled = true;  
+        defaultOption.selected = true;  
+        defaultOption.textContent = '';
+        selectCat.appendChild(defaultOption);
+                    
+        categories.forEach(cat => {
+        const option = document.createElement('option');
+        option.value = cat.id;
+        option.textContent = cat.name;
+        selectCat.appendChild(option);
+        });
+        } catch (error) {
+            console.error("Erreur : ", error);
         }
+    }
+fetchCategoriesModal();
+       
+function resetForm() {
+    formAddTitle.value = '';
+    selectCat.selectedIndex = 0;
             
-    addBtn.addEventListener('click', () => {
-
-        galleryModalContent.setAttribute('style', 'display: none');
-        addPictureModalContent.setAttribute('style', 'display: flex');
-
-        resetForm();
-        checkFormValidity();
-    });
-
-    btnReturn.addEventListener('click', () => {
-
-        galleryModalContent.setAttribute('style', 'display: flex');
-        addPictureModalContent.setAttribute('style', 'display: none');
-        errorMessage.style.display = "none";
-        errorMessage.textContent = "";
-
-        resetForm();
-        checkFormValidity();
-    });
-
-    const previewImg = document.getElementById('preview-img');
-
-    function validTypeFiles(file) {
-    const fileTypes = ['image/jpg', 'image/png'];
-    return fileTypes.includes(file.type);
+    const oldPreviewImage = addPictureWrapper.querySelector('img');
+    if (oldPreviewImage) {
+        oldPreviewImage.remove();
     }
 
-    function newImageAddDisplay (event) {
-        event.preventDefault();
-        
-        const file = event.target.files[0];
-        previewImg.innerHTML = '';
+    pictureIcon.setAttribute('style', 'display: flex');
+    labelBtnFile.setAttribute('style', 'display: flex');
+    pictureInstructionText.setAttribute('style', 'display: flex');
+}
+            
+addBtn.addEventListener('click', () => {
 
-        const pictureIcone = document.querySelector('.icon-picture');
-        const labelBtnFile = document.querySelector('label[for="fileInput"]');
-        const txtAddPicture = document.querySelector('.txt-wrapper-picture');
+    galleryModalContent.setAttribute('style', 'display: none');
+    addPictureModalContent.setAttribute('style', 'display: flex');
+
+    resetForm();
+    checkFormValidity();
+});
+
+btnReturn.addEventListener('click', () => {
+    galleryModalContent.setAttribute('style', 'display: flex');
+    addPictureModalContent.setAttribute('style', 'display: none');
+    errorMessage.style.display = "none";
+    errorMessage.textContent = "";
+
+    resetForm();
+    checkFormValidity();
+});
+
+const previewImg = document.getElementById('preview-img');
+
+function validTypeFiles(file) {
+const fileTypes = ['image/jpg', 'image/png'];
+return fileTypes.includes(file.type);
+}
+
+function newImageAddDisplay (event) {
+    event.preventDefault();
+        
+    const file = event.target.files[0];
+    previewImg.innerHTML = '';
+
+    const pictureIcone = document.querySelector('.icon-picture');
+    const labelBtnFile = document.querySelector('label[for="fileInput"]');
+    const txtAddPicture = document.querySelector('.txt-wrapper-picture');
 
     if (!file) {
         const textAddImg = document.createElement('p');
@@ -322,26 +320,26 @@ async function deleteProject(event) {
         pictureIcone.setAttribute('style', 'display: flex');
         labelBtnFile.setAttribute('style', 'display: flex');
         txtAddPicture.setAttribute('style', 'display: flex');
-    }}
-      btnAddPicture.addEventListener('change', newImageAddDisplay);
+}}
+btnAddPicture.addEventListener('change', newImageAddDisplay);
       
 async function sendNewPicture(event) {
-        event.preventDefault();
-        const token = localStorage.getItem("token");
-        const idCat = selectCat.value;
-        const title = formAddTitle.value;
-        const file = btnAddPicture.files[0];
+    event.preventDefault();
+    const token = localStorage.getItem("token");
+    const idCat = selectCat.value;
+    const title = formAddTitle.value;
+    const file = btnAddPicture.files[0];
 
     if (!idCat || !title || !file) {
-            errorMessage.textContent = "Veuillez remplir tous les champs.";
-            errorMessage.style.display = "block";
-            return;
-        }
+        errorMessage.textContent = "Veuillez remplir tous les champs.";
+        errorMessage.style.display = "block";
+        return;
+    }
         
-        const formData = new FormData();
-        formData.append('image', file);
-        formData.append('title', title);
-        formData.append('category', idCat);
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('title', title);
+    formData.append('category', idCat);
 
     try {
         const response = await fetch(urlAdmin + 'works', {
@@ -352,35 +350,35 @@ async function sendNewPicture(event) {
             body: formData,
         });
 
-        if (!response.ok) {
-            throw new Error("Erreur lors de l'ajout du projet.");
-        }
-
-        const newProject = await response.json();
-            console.log("Projet ajouté avec succès", newProject);
-            closeModal(event);
-            addProjectInGallery(newProject);
-            addProjectInModal(newProject);
-            errorMessage.style.display = "none";
-            errorMessage.textContent = "";
-            
-        } catch (error) {
-            console.error("Erreur : ", error);
-        }
+    if (!response.ok) {
+        throw new Error("Erreur lors de l'ajout du projet.");
     }
-    function checkFormValidity() {
-        if (formAddTitle.value && selectCat.value && btnAddPicture.files.length > 0) {
-            btnAddNewPicture.classList.add('active');
-        } else {
-            btnAddNewPicture.classList.remove('active');
-        }
+
+    const newProject = await response.json();
+    console.log("Projet ajouté avec succès", newProject);
+    closeModal(event);
+    addProjectInGallery(newProject);
+    addProjectInModal(newProject);
+    errorMessage.style.display = "none";
+    errorMessage.textContent = "";
+            
+    } catch (error) {
+        console.error("Erreur : ", error);
+    }
+}
+function checkFormValidity() {
+    if (formAddTitle.value && selectCat.value && btnAddPicture.files.length > 0) {
+        btnAddNewPicture.classList.add('active');
+    } else {
+        btnAddNewPicture.classList.remove('active');
+    }
     }
     formAddTitle.addEventListener('input', checkFormValidity);
     selectCat.addEventListener('change', checkFormValidity);
     btnAddPicture.addEventListener('change', checkFormValidity);
     
     btnAddNewPicture.addEventListener('click', sendNewPicture);
-
+   
 const adminConnexion = localStorage.getItem("adminEmail") === "sophie.bluel@test.tld";
 
     if (adminConnexion) {
--- FrontEnd/scripts/connexion.js
+++ FrontEnd/scripts/connexion.js
@@ -1,7 +1,7 @@
 
 let urlLogin = 'http://localhost:5678/api/';
 
- function sendLogin() {
+function sendLogin() {
     const formConnexion = document.querySelector(".login-form");
 
     if (!formConnexion) return;
@@ -9,50 +9,49 @@ let urlLogin = 'http://localhost:5678/api/';
     formConnexion.addEventListener("submit", async function (event) {
         event.preventDefault();
        
-        const loginData = {
-            email : event.target.querySelector("[name=email]").value,
-            password : event.target.querySelector("[name=password]").value,
-        };
+    const loginData = {
+        email : event.target.querySelector("[name=email]").value,
+        password : event.target.querySelector("[name=password]").value,
+    };
 
-        const utileCharge = JSON.stringify(loginData);
-        localStorage.setItem('adminEmail', loginData.email);
+    const utileCharge = JSON.stringify(loginData);
+    localStorage.setItem('adminEmail', loginData.email);
 
     let errorMessage = document.querySelector(".error-message");
-        if (!errorMessage) {
-            errorMessage = document.createElement("div");
-            errorMessage.classList.add("error-message");
-            errorMessage.style.display = "none";
-            formConnexion.appendChild(errorMessage);
-        }
+    if (!errorMessage) {
+        errorMessage = document.createElement("div");
+        errorMessage.classList.add("error-message");
         errorMessage.style.display = "none";
+        formConnexion.appendChild(errorMessage);
+    }
+    errorMessage.style.display = "none";
         
-        try {
-            const response = await fetch(urlLogin + 'users/login', {
+    try {
+        const response = await fetch(urlLogin + 'users/login', {
     
-            method : "POST",
-            headers : { "Content-Type" : "application/json" },
-            body : utileCharge,
-        });
+        method : "POST",
+        headers : { "Content-Type" : "application/json" },
+        body : utileCharge,
+    });
         
     if (response.ok) {
-            const data = await response.json();
-            const token = data.token;
-            const email = loginData.email;
-            console.log("Token reçu :", token);
+        const data = await response.json();
+        const token = data.token;
+        const email = loginData.email;
+        console.log("Token reçu :", token);
             
-            localStorage.setItem("token", token);   
-            console.log("Token enregistré dans le stockage local.");
+        localStorage.setItem("token", token);   
+        console.log("Token enregistré dans le stockage local.");
         
-        if (email === "sophie.bluel@test.tld") {
-            window.location.href = "index.html";
+    if (email === "sophie.bluel@test.tld") {
+        window.location.href = "index.html";
            
-
-        } else {
-            window.location.href = "index.html";
+    } else {
+        window.location.href = "index.html";
         }} 
-        else {
-            errorMessage.style.display = "block";
-            errorMessage.innerText = "Erreur dans l’identifiant ou le mot de passe";
+    else {
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Erreur dans l’identifiant ou le mot de passe";
         }
     } catch (error) {
         errorMessage.style.display = "block";
@@ -60,7 +59,7 @@ let urlLogin = 'http://localhost:5678/api/';
     }
 
     formConnexion.reset(); 
-    });}
+});}
 
 sendLogin();
 
